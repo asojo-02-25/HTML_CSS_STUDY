35918ec05873499cc7ebc0358e7c3c0b3f658ab4
Jo <joutarouasou@gmail.com>
2025-10-04 11:52:19 +0000

    表示の不具合を解決 cssで#menu imgタグにwidthのプロパティを追加することで解決しました

--- chapter5/js/script.js
+++ chapter5/js/script.js
@@ -1,13 +1,10 @@
 const menu = document.querySelector('#menu');
 const list = ['94.png', '107.png', '109.png', '113.png', '116.png', '117.png']; 
 
-const content = `<div><img src="images/${list[0]}" alt="pic94"></div>
-<div><img src="images/${list[1]}" alt="pic107"></div>
-<div><img src="images/${list[2]}" alt="pic109"></div>
-<div><img src="images/${list[3]}" alt="pic113"></div>
-<div><img src="images/${list[4]}" alt="pic116"></div>
-<div><img src="images/${list[5]}" alt="pic117"></div>
-`; 
+for(let i = 0; i < list.length; i++){
+    const content = `<div><img src="images/${list[i]}" alt=${list[i]}div>`;
+    menu.insertAdjacentHTML('beforeend',content);
+}
 
-menu.insertAdjacentHTML('beforeend',content);
+
 
