772068cd5380968f593953b363cc4798064d32bc
Jo <joutarouasou@gmail.com>
2025-10-16 03:17:42 +0000

    loadingに関して、動作確認終了

--- chapter7/js/script.js
+++ chapter7/js/script.js
@@ -3,16 +3,22 @@ const loadingAreaGray = document.querySelector('#loading')
 const loadingAreaGreen = document.querySelector('#loading-screen')
 const loadingText = document.querySelector('#loading p')
 
-window.addEventListener('load' = () =>{
+window.addEventListener('load', () =>{
     //ローディング終了後、グレースクリーンを透明にする
     loadingAreaGray.animate(
-        {
-            opacity: [1, 0],
-            visibility: 'hidden',
-        },
+        [
+            {
+                opacity: 1,
+                offset: .8,
+            },
+            {
+                opacity: 0,
+                offset: 1,
+                visibility: 'hidden',
+            },
+        ],
         {
             duration: 2000,
-            delay: 1200,
             easing: 'ease',
             fill: 'forwards',  
         }
@@ -38,7 +44,7 @@ window.addEventListener('load' = () =>{
     //ローディング中、グリーンスクリーンを100vh(画面下側)から-100vh(画面上側)まで移動させる(コンピューターの世界では左上を座標の(0, 0)としている)
     loadingAreaGreen.animate(
         {
-            translate: ['0, 100vh', '0, 0', '0, -100vh']
+            translate: ['0 100vh', '0 0', '0 -100vh']
         },
         {
             duration: 2000,
